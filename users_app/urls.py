2ac00ce695cf1e18da077feb530ae2e2cc8b7e4a
TagRomEdu <qwertystyles@gmail.com>
2024-01-15 13:44:05 +0500

    added serializer for User's creation and changed way to jwt

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,10 +1,8 @@
 from django.urls import include, path
 from djoser.views import UserViewSet
 from rest_framework.routers import SimpleRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
+
 from users_app.apps import UsersAppConfig
 
 
@@ -13,9 +11,6 @@ app_name = UsersAppConfig.name
 user_router = SimpleRouter()
 user_router.register('users', UserViewSet, basename='users')
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(),
-         name='token_refresh'),
     path('', include(user_router.urls)),
+    path('', include('djoser.urls.jwt')),
 ]

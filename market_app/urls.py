efb5db3ce7ff48dd1eaaf7741b311d1c32bf6e96
TagRomEdu <qwertystyles@gmail.com>
2024-01-15 22:34:52 +0500

    added views and urls for ads and reviews

--- market_app/urls.py
+++ market_app/urls.py
@@ -1,9 +1,17 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from market_app.apps import MarketAppConfig
+from market_app.views import AdvertisementViewSet
 
 
 app_name = MarketAppConfig.name
 
-urlpatterns = [
+ads_router = DefaultRouter()
+ads_router.register(r'ads', AdvertisementViewSet, basename='ads')
+reviews_router = DefaultRouter()
+reviews_router.register(r'reviews', AdvertisementViewSet, basename='reviews')
 
+urlpatterns = [
+    path('', include(ads_router.urls)),
+    path('', include(reviews_router.urls)),
 ]
--- market_app/views.py
+++ market_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from market_app.models import Advertisement, Review
+from market_app.paginators import AdvertisementPaginator
 
-# Create your views here.
+from market_app.serializers import AdvertisementSerializer, ReviewSerializer
+
+
+class AdvertisementViewSet(viewsets.ModelViewSet):
+    serializer_class = AdvertisementSerializer
+    queryset = Advertisement.objects.all()
+    pagination_class = AdvertisementPaginator
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    queryset = Review.objects.all()

c6779f7a074d07f233adaa3ce57a53e8965d5b0b
TagRomEdu <qwertystyles@gmail.com>
2024-01-18 13:11:39 +0500

    changed serializer for creating users - now with caching password

--- users_app/serializers.py
+++ users_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.contrib.auth import get_user_model
 from users_app.models import User
 
 
@@ -7,3 +7,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        return user

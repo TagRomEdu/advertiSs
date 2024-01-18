9e170f3fdbeb09013cbf0884423789d29d7b169d
TagRomEdu <qwertystyles@gmail.com>
2024-01-18 12:01:52 +0500

    fixed with PEP8

--- market_app/urls.py
+++ market_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from market_app.apps import MarketAppConfig
-from market_app.views import AdsListAPIView, AdvertisementViewSet, ReviewViewSet
+from market_app.views import (AdsListAPIView, AdvertisementViewSet,
+                              ReviewViewSet)
 
 
 app_name = MarketAppConfig.name

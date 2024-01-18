5a95cee42387db937fc568a5627bc27b9b9e810f
TagRomEdu <qwertystyles@gmail.com>
2024-01-18 11:56:36 +0500

    fixed controller for reviews urls

--- market_app/urls.py
+++ market_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from market_app.apps import MarketAppConfig
-from market_app.views import AdsListAPIView, AdvertisementViewSet
+from market_app.views import AdsListAPIView, AdvertisementViewSet, ReviewViewSet
 
 
 app_name = MarketAppConfig.name
@@ -9,7 +9,7 @@ app_name = MarketAppConfig.name
 ads_router = DefaultRouter()
 ads_router.register(r'ads', AdvertisementViewSet, basename='ads')
 reviews_router = DefaultRouter()
-reviews_router.register(r'reviews', AdvertisementViewSet, basename='reviews')
+reviews_router.register(r'reviews', ReviewViewSet, basename='reviews')
 
 urlpatterns = [
     path('', include(ads_router.urls)),
